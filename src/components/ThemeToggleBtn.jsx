594a2fd6648df6f0c60b226dec98265d757febb1
Oluwatosin <tosindaniel360@gmail.com>
2025-08-18 19:20:41 +0100

    first commit

---
+++ src/components/ThemeToggleBtn.jsx
@@ -0,0 +1,35 @@
+import React, { useEffect } from 'react'
+import assets from '../assets/assets'
+
+const ThemeToggleBtn = ({ theme, setTheme }) => {
+
+    // Initialize theme from localStorage or system preference
+    useEffect(() => {
+        const preferDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
+        setTheme(theme || (preferDarkMode ? 'dark' : 'light' ));
+    });
+
+    // Update the document class and localStorage when theme changes
+    useEffect(() => {
+        if (theme === 'dark') {
+            document.documentElement.classList.add('dark')
+        }else {
+            document.documentElement.classList.remove('dark')
+        }
+        localStorage.setItem('theme', theme)
+    }, [theme])
+
+    return (
+        <>
+            <button className='cursor-pointer'>
+                {theme === 'dark' ? (
+                    <img onClick={() => setTheme('light')} src={assets.sun_icon} alt="Light-Theme" className='size-8.5 p-1.5 border border-gray-500 rounded-full' />
+                ) : (
+                    <img onClick={() => setTheme('dark')} src={assets.moon_icon} alt="Dark-Theme" className='size-8.5 p-1.5 border border-gray-500 rounded-full' />
+                )}
+            </button>
+        </>
+    )
+}
+
+export default ThemeToggleBtn
